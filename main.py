888bb89c156c9fd4a55485abc6dabf60d22dca57
reveriePolaris <sean.cardenas2@gmail.com>
2025-09-27 14:12:03 -0400

    Edit: initialized flask page

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello world")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def landing_page():
+    return "Welcome to our landing page!"
+
+if __name__ == "__main__":
+    app.run()
