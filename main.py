0c2f633dc6642436a51dd91602cd67e225061ce6
reveriePolaris <sean.cardenas2@gmail.com>
2025-09-27 14:27:20 -0400

    added folders and template path

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+dir_path = os.path.dirname(__file__)
+folder_path = os.path.join(dir_path, "html")
+
+app = Flask(__name__, template_folder = folder_path)
 
 @app.route("/")
 def landing_page():
-    return "Welcome to our landing page!"
+    return render_template("index.html")
+
+@app.route("/itinerary")
+def show_iternary():
+    return render_template("itinerary.html")
 
 if __name__ == "__main__":
     app.run()
